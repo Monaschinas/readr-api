f8259c68a6b8e5959283b8e3d2793b1ea27d4307
aleiva <aleiva1700@gmail.com>
2023-06-01 16:28:46 -0500

    refactor: remove unused imports

--- src/main/java/com/monaschinas/readr/platform/profile/domain/persistence/RoleRepository.java
+++ src/main/java/com/monaschinas/readr/platform/profile/domain/persistence/RoleRepository.java
@@ -1,8 +1,6 @@
 package com.monaschinas.readr.platform.profile.domain.persistence;
 
 import com.monaschinas.readr.platform.profile.domain.model.Role;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
