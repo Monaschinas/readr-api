742869cecf3474632bc0b17e33ba360361b1506a
aleiva <aleiva1700@gmail.com>
2023-06-01 10:02:24 -0500

    Initial Commit

---
+++ src/main/java/com/monaschinas/readr/platform/ReadrApplication.java
@@ -0,0 +1,13 @@
+package com.monaschinas.readr.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/monaschinas/readr/platform/ReadrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monaschinas.readr.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
