7350bbb54b68e5cd12524e6a7979c1e583addc40
aleiva <aleiva1700@gmail.com>
2023-06-01 17:24:42 -0500

    feat: add role mapper with mapping configuration

---
+++ src/main/java/com/monaschinas/readr/platform/profile/mapping/MappingConfiguration.java
@@ -0,0 +1,12 @@
+package com.monaschinas.readr.platform.profile.mapping;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration("profileMappingConfiguration")
+public class MappingConfiguration {
+  @Bean
+  public RoleMapper roleMapper() {
+    return new RoleMapper();
+  }
+}
