be6253686b9cf4e2a765ae7a72be7c51cb4dcd3b
Stevens <u20201e735@upc.edu.pe>
2023-06-02 02:36:27 -0500

    feat: add language entity

---
+++ src/main/java/com/monaschinas/readr/platform/publishing/domain/model/Language.java
@@ -0,0 +1,30 @@
+package com.monaschinas.readr.platform.publishing.domain.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@With
+@Entity
+@Table(name = "languages")
+public class Language {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 12)
+    private String name;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 4)
+    private String abbreviation;
+}
