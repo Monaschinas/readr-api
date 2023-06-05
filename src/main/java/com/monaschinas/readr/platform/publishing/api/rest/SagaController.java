a106da36bd910d51ca6796fe92c4a17cdf6ca0d8
jhuan <j.huanca4141@gmail.com>
2023-06-05 17:11:20 -0500

    feat: add saga and saga status controller

---
+++ src/main/java/com/monaschinas/readr/platform/publishing/api/rest/SagaController.java
@@ -0,0 +1,47 @@
+package com.monaschinas.readr.platform.publishing.api.rest;
+
+import com.monaschinas.readr.platform.publishing.domain.service.SagaService;
+import com.monaschinas.readr.platform.publishing.mapping.SagaMapper;
+import com.monaschinas.readr.platform.publishing.resource.CreateSagaResource;
+import com.monaschinas.readr.platform.publishing.resource.SagaResource;
+import com.monaschinas.readr.platform.publishing.resource.UpdateSagaResource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/api/v1/sagas", produces = "application/json")
+public class SagaController {
+    private final SagaService sagaService;
+    private final SagaMapper mapper;
+
+    public SagaController(SagaService sagaService, SagaMapper mapper) {
+        this.sagaService = sagaService;
+        this.mapper = mapper;
+    }
+    @GetMapping
+    public Page<SagaResource> getAllSagas(Pageable pageable){
+        return mapper.modelListPage(sagaService.getAll(),pageable);
+    }
+
+    @GetMapping({"sagaId"})
+    public SagaResource getSagaById(@PathVariable Long sagaId){
+        return mapper.toResource(sagaService.getById(sagaId));
+    }
+
+    @PostMapping
+    public SagaResource createSaga(@RequestBody CreateSagaResource resource){
+        return mapper.toResource(sagaService.create(mapper.toModel(resource)));
+    }
+
+    @PutMapping({"sagaId"})
+    public SagaResource updateSaga(@PathVariable Long sagaId, @RequestBody UpdateSagaResource resource){
+        return mapper.toResource(sagaService.update(sagaId, mapper.toModel(resource)));
+    }
+
+    @DeleteMapping({"sagaId"})
+    public ResponseEntity<?> deleteSaga(@PathVariable Long sagaId){
+        return sagaService.delete(sagaId);
+    }
+}
