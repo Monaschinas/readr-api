950af401d48f4fc32bc9c4aeaa86e90c47141eb8
kitagawaowo <uwu20201b980@gmail.com>
2023-06-06 09:55:15 -0500

    fix: add decorator for chapter service implementation

--- src/main/java/com/monaschinas/readr/platform/publishing/service/ChapterServiceImpl.java
+++ src/main/java/com/monaschinas/readr/platform/publishing/service/ChapterServiceImpl.java
@@ -11,11 +11,13 @@ import jakarta.validation.Validator;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
+@Service
 public class ChapterServiceImpl implements ChapterService {
     private static final String ENTITY = "Chapter";
 
