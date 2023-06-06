d98db5100f48d8678f181d504ff4a516dd8c0ed1
kitagawaowo <uwu20201b980@gmail.com>
2023-06-06 09:33:38 -0500

    fix: add missing method for chapter repository

--- src/main/java/com/monaschinas/readr/platform/publishing/domain/persistence/ChapterRepository.java
+++ src/main/java/com/monaschinas/readr/platform/publishing/domain/persistence/ChapterRepository.java
@@ -1,6 +1,8 @@
 package com.monaschinas.readr.platform.publishing.domain.persistence;
 
 import com.monaschinas.readr.platform.publishing.domain.model.Chapter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +11,5 @@ import java.util.Optional;
 public interface ChapterRepository extends JpaRepository<Chapter, Long> {
     Optional<Chapter> findByTitle(String title);
     List<Chapter> findByBookId(Long bookId);
+    Page<Chapter> findByBookId(Pageable pageable, Long bookId);
 }
