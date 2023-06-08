835d59ae93c34b91612a86f89ec922568c9367fa
aleiva <aleiva1700@gmail.com>
2023-06-08 16:27:43 -0500

    refactor: rename path from genre controller

--- src/main/java/com/monaschinas/readr/platform/publishing/api/rest/GenreController.java
+++ src/main/java/com/monaschinas/readr/platform/publishing/api/rest/GenreController.java
@@ -11,38 +11,38 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/api/v1/genres", produces = "application/json")
+@RequestMapping(value = "/api/v1/publishing/genres", produces = "application/json")
 public class GenreController {
-    private final GenreService genreService;
-    private final GenreMapper mapper;
+  private final GenreService genreService;
+  private final GenreMapper mapper;
 
-    public GenreController(GenreService genreService, GenreMapper mapper) {
-        this.genreService = genreService;
-        this.mapper = mapper;
-    }
+  public GenreController(GenreService genreService, GenreMapper mapper) {
+    this.genreService = genreService;
+    this.mapper = mapper;
+  }
 
-    @GetMapping
-    public Page<GenreResource> getAllGenres(Pageable pageable){
-        return mapper.modelListPage(genreService.getAll(),pageable);
-    }
+  @GetMapping
+  public Page<GenreResource> getAllGenres(Pageable pageable){
+    return mapper.modelListPage(genreService.getAll(),pageable);
+  }
 
-    @GetMapping({"genreId"})
-    public GenreResource getGenreById(@PathVariable Long genreId){
-        return mapper.toResource(genreService.getById(genreId));
-    }
+  @GetMapping({"genreId"})
+  public GenreResource getGenreById(@PathVariable Long genreId){
+    return mapper.toResource(genreService.getById(genreId));
+  }
 
-    @PostMapping
-    public GenreResource createGenre(@RequestBody CreateGenreResource resource){
-        return mapper.toResource(genreService.create(mapper.toModel(resource)));
-    }
+  @PostMapping
+  public GenreResource createGenre(@RequestBody CreateGenreResource resource){
+    return mapper.toResource(genreService.create(mapper.toModel(resource)));
+  }
 
-    @PutMapping({"genreId"})
-    public GenreResource updateGenre(@PathVariable Long genreId, @RequestBody UpdateGenreResource resource){
-        return mapper.toResource(genreService.update(genreId, mapper.toModel(resource)));
-    }
+  @PutMapping({"genreId"})
+  public GenreResource updateGenre(@PathVariable Long genreId, @RequestBody UpdateGenreResource resource){
+    return mapper.toResource(genreService.update(genreId, mapper.toModel(resource)));
+  }
 
-    @DeleteMapping({"genreId"})
-    public ResponseEntity<?> deleteGenre(@PathVariable Long genreId){
-        return genreService.delete(genreId);
-    }
+  @DeleteMapping({"genreId"})
+  public ResponseEntity<?> deleteGenre(@PathVariable Long genreId){
+    return genreService.delete(genreId);
+  }
 }
